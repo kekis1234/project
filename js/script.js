03d72f03f32c90b8735ffe24127d6eb3de435d10
misha <example@gmail.com>
2024-11-06 14:14:01 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,50 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-const lastWatched = prompt('Один из последних просмотренных фильмов?');
-const itsRating = prompt('На сколько оцените его?');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+// const lastWatched = prompt('Один из последних просмотренных фильмов?');
+// const itsRating = prompt('На сколько оцените его?');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-        [lastWatched]: itsRating
-    },
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {
+//         [lastWatched]: itsRating
+//     },
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+// console.log(personalMovieDB)
+
+
+// if (4 == 9) {
+//    console.log('Ok!'); 
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Mnogo');
+//  }else {
+//     console.log('Ok!');
+//  }
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('neverno');
+        break;
+    case 100:
+        console.log('neverno');
+        break;
+    case 50:
+        console.log('в точку!');
+        break;
+    default:
+        console.log('не сегодня');
+        break;
 }
-console.log(personalMovieDB)
